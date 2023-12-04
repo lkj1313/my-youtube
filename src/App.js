a77184a90570eaa97f75ff473ae272533ac8ac4d
KJ <smm45108@gmail.com>
2023-12-04 14:53:03 +0900

    섹션 컴퍼넌트 구조화하기

--- src/App.js
+++ src/App.js
@@ -12,24 +12,31 @@ import Channel from "./pages/Channel";
 import Video from "./pages/Video";
 import Search from "./pages/Search";
 import Not from "./pages/Not";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/today" element={<Today />}></Route>
-        <Route path="/developer" element={<Developer />}></Route>
-        <Route path="/webd" element={<Webd />}></Route>
-        <Route path="/website" element={<Website />}></Route>
-        <Route path="/gsap" element={<Gsap />}></Route>
-        <Route path="/port" element={<Port />}></Route>
-        <Route path="/youtube" element={<Youtube />}></Route>
-        <Route path="/channel/:channelID" element={<Channel />}></Route>
-        <Route path="/video/:videoID" element={<Video />}></Route>
-        <Route path="/search/:searchID" element={<Search />}></Route>
-        <Route path="*" element={<Not />}></Route>
-      </Routes>
+      <Header />
+      <Main>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/today" element={<Today />}></Route>
+          <Route path="/developer" element={<Developer />}></Route>
+          <Route path="/webd" element={<Webd />}></Route>
+          <Route path="/website" element={<Website />}></Route>
+          <Route path="/gsap" element={<Gsap />}></Route>
+          <Route path="/port" element={<Port />}></Route>
+          <Route path="/youtube" element={<Youtube />}></Route>
+          <Route path="/channel/:channelID" element={<Channel />}></Route>
+          <Route path="/video/:videoID" element={<Video />}></Route>
+          <Route path="/search/:searchID" element={<Search />}></Route>
+          <Route path="*" element={<Not />}></Route>
+        </Routes>
+      </Main>
+      <Footer />
     </BrowserRouter>
   );
 };
